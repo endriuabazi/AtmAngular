301e6acbeb1b4fe6ad36266e6f45a78dedafe250
Endriu <endriu.abazi@greenleaf-bsc.com>
2023-02-04 09:31:50 +0100

    Edit profile added

--- src/app/Screens/edit-profile/edit-profile.component.ts
+++ src/app/Screens/edit-profile/edit-profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
+import { CartsService } from 'src/app/Services/carts.service';
+import { ClientService } from 'src/app/Services/client.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,9 +12,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent  implements OnInit{
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  myForm: FormGroup | any;
+  userName:any;
+  surname:any;
+  name:any;
+  phone:any;
+  age:any;
+  email:any;
+  hasFormErrors = false;
+  loginSuccesFlag = false;
+  hide = true;
+  constructor(
+    private authService: AuthService,
+    private userFB: FormBuilder,
+    public dialog: MatDialog,
+    private route: ActivatedRoute , 
+   private clientService: ClientService,
+    private router: Router
+  ) {}
+ 
+
+  isDisabled = true; // or true, depending on your desired initial state
+
+  ngOnInit() {
+    this.userName = localStorage.getItem('username');
+
+   const bisha= this.clientService.getClientbyUsername(this.userName).subscribe((data:any) => {
+      this.surname = data[0].surname;
+      this.name= data[0].name;
+      this.email = data[0].email;
+      this.age = data[0].age;
+      this.phone = data[0].phone;
+      console.log(data);
+
+    
+ // Create form
+ this.myForm = this.userFB.group({
+  username: [this.userName],
+  name: [this.name],
+  surname: [this.surname],
+
+  email: [this.email],
+  phone: [this.phone],
+
+  age: [this.age],
+
+
+});
+    });
+
+
   }
-  username = localStorage.getItem('username');
+
+  onSubmit() {
+    const data = {
+     
+      name: this.myForm.value.name,
+      surname: this.myForm.value.surname,
+      email: this.myForm.value.email,
+      phone: this.myForm.value.phone,
+      username: this.myForm.value.username,
+      age: this.myForm.value.age,
+  
+ 
+    };
+
+    this.clientService.editProfile(this.userName,data)
+  }
+  
 
 }
---
+++ src/app/Services/client.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+ 
+ 
+  constructor(private http: HttpClient,private toastr: ToastrService,  private router: Router) { }
+  private baseUrl = `${environment.backendUrl}`;
+
+  
+  getClientbyUsername(username: string){
+
+    return this.http.get(`${this.baseUrl}/api/Client/GetClientByUsername?username=${username}`);
+    }
+
+  // api/Client/editProfile?username=buci
+editProfile(username: string,data:any){
+
+  fetch(`${this.baseUrl}/api/Client/editProfile?username=${username}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (response.status === 200) {
+        const userName = localStorage.getItem('username');
+        this.toastr.success('Profile edited success!');
+        this.router.navigate(['/home',username]);
+        return response.json();
+      } else {
+        this.toastr.error("Profile edit failed.");
+        throw new Error('Something went wrong');
+        
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    
+    });
+
+}
+}
